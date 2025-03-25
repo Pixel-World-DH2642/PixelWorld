7aa4194ee296a006dfdeba4673e13000f913134a
Lambozhuang <Lambozhuang@gmail.com>
2025-03-25 13:19:08 +0100

    Add all four direction inputs

--- src/index.jsx
+++ src/index.jsx
@@ -17,8 +17,32 @@ reactiveModel.inputHandler.addBinding(
     console.log(reactiveModel.position);
     reactiveModel.position.x += 10;
   },
-  UI_INPUT_TYPE.CODE_DOWN,
+  UI_INPUT_TYPE.KEY_DOWN,
   "ArrowRight",
+);
+reactiveModel.inputHandler.addBinding(
+  () => {
+    console.log(reactiveModel.position);
+    reactiveModel.position.x -= 10;
+  },
+  UI_INPUT_TYPE.CODE_DOWN,
+  "ArrowLeft",
+);
+reactiveModel.inputHandler.addBinding(
+  () => {
+    console.log(reactiveModel.position);
+    reactiveModel.position.y -= 10;
+  },
+  UI_INPUT_TYPE.CODE_DOWN,
+  "ArrowUp",
+);
+reactiveModel.inputHandler.addBinding(
+  () => {
+    console.log(reactiveModel.position);
+    reactiveModel.position.y += 10;
+  },
+  UI_INPUT_TYPE.CODE_DOWN,
+  "ArrowDown",
 );
 
 createRoot(document.getElementById("root")).render(
