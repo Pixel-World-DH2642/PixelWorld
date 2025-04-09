6665b4b311c2871a0d9f777bb16bebc72fdd15c4
Lambozhuang <lambozhuang@gmail.com>
2025-04-09 20:32:08 +0200

    Add tailwindcss and update vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import reactJsxPlugin from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [reactJsxPlugin()],
+  plugins: [reactJsxPlugin(), tailwindcss()],
   server: {
     port: 8080,
   },
