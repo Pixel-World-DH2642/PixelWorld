27b05d1a2f7ece701c94e19bac71b131ddd7c33f
Lambozhuang <Lambozhuang@gmail.com>
2025-04-13 00:59:45 +0200

    Match the painting mock to museum page

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,6 +1,29 @@
 import { Link } from "react-router-dom";
 
 export function DetailPage({ painting, onLikePainting, onDislikePainting }) {
+  painting = {
+    id: "painting1",
+    title: "Beauty",
+    colorMatrix: Array(32)
+      .fill()
+      .map(() =>
+        Array(32)
+          .fill()
+          .map(
+            () =>
+              "#" +
+              Math.floor(Math.random() * 16777215)
+                .toString(16)
+                .padStart(6, "0"),
+          ),
+      ),
+    savedQuote: "Art washes away from the soul the dust of everyday life.",
+    author: "PicassoFan123",
+    date: Date.now() - 100000000,
+    authorNotes: "Inspired by the colors of a Spanish sunset.",
+    likedBy: ["user1", "user2", "user3"],
+  };
+
   return (
     <div className="font-pixel p-6 mx-auto w-[512px] md:w-[768px] lg:w-[1024px]">
       <Link
@@ -11,7 +34,28 @@ export function DetailPage({ painting, onLikePainting, onDislikePainting }) {
         <div className="pl-4 hover:underline flex text-3xl">Back to museum</div>
       </Link>
       <div className="flex flex-col md:flex-row">
-        <div className="flex-shrink-0 aspect-square md:w-1/2 md:h-1/2 bg-gray-500 rounded-md shadow-md border mb-4"></div>
+        <div className="flex-shrink-0 aspect-square md:w-1/2 md:h-1/2 bg-gray-500 border-2 mb-4">
+          {painting.colorMatrix.map((row, rowIndex) => (
+            <div
+              key={rowIndex}
+              className="flex"
+              style={{
+                height: `${100 / painting.colorMatrix.length}%`,
+              }}
+            >
+              {row.map((color, colIndex) => (
+                <div
+                  key={`${rowIndex}-${colIndex}`}
+                  style={{
+                    backgroundColor: color,
+                    width: `${100 / row.length}%`,
+                    height: "100%",
+                  }}
+                />
+              ))}
+            </div>
+          ))}
+        </div>
         <div className="md:px-4">
           <h1 className="text-4xl pb-1">Title</h1>
           <div className="pb-2">
