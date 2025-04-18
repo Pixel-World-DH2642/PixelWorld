a44ffffca275c7efde1fab7579b352c9594267e8
Krisztina biro <kbiro@kth.se>
2025-04-18 10:07:19 +0200

    .

--- src/containers/Museum.jsx
+++ src/containers/Museum.jsx
@@ -1,11 +1,30 @@
 import { connect } from "react-redux";
 import { MuseumPage } from "../pages/MuseumPage";
 
+import { 
+  selectPainting, 
+  nextPaintings, 
+  prevPaintings,
+  selectAllPaintings,
+  selectCurrentPaintings,
+  selectIsFirstPage,
+  selectIsLastPage
+} from "../pages/MuseumPage";
+
 export const Museum = connect(
   function mapStateToProps(state) {
-    return {};
+    return {
+      paintings: selectAllPaintings(state),
+      currentPaintings: selectCurrentPaintings(state),
+      isFirstPage: selectIsFirstPage(state),
+      isLastPage: selectIsLastPage(state)
+    };
   },
   function mapDispatchToProps(dispatch) {
-    return {};
+    return {
+      onSelectPainting: (id) => dispatch(selectPainting(id)),
+      onNextClick: () => dispatch(nextPaintings()),
+      onPrevClick: () => dispatch(prevPaintings())
+    };
   },
 )(MuseumPage);
