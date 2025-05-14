bed562f3c17f6c25266ad7b097147e0109d33552
Lambozhuang <Lambozhuang@gmail.com>
2025-05-15 00:00:11 +0200

    fix: login page padding

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -72,136 +72,138 @@ export function LoginPage({ onLogin, onLoginWithGoogle, onSignup }) {
   };
 
   return (
-    <div className="">
-      {!showSignup ? (
-        <>
-          <h1 className="text-3xl font-pixel font-bold mb-6 text-center flex justify-between items-center">
-            <div className="text-left">Log in to Pixelworld</div>
-            <span className="inline-block ml-2">🖌️</span>
-          </h1>
-          <form className="flex flex-col gap-4" onSubmit={handleLoginSubmit}>
-            <div>
-              <label className="block text-sm mb-1">Email Address</label>
-              <input
-                type="email"
-                className="w-full border border-gray-300 rounded-md p-2"
-                placeholder="Email Address"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </div>
-            <div>
-              <label className="block text-sm mb-1">Password</label>
-              <input
-                type="password"
-                className="w-full border border-gray-300 rounded-md p-2"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </div>
-            <button
-              type="submit"
-              className="bg-gray-500 text-white rounded-md py-2 hover:bg-gray-600 border-1 border-gray-800 cursor-pointer"
-            >
-              Log in
-            </button>
-          </form>
+    <div className="max-h-[calc(100vh-4rem)] px-8 pt-8">
+      <div className="pb-8">
+        {!showSignup ? (
+          <>
+            <h1 className="text-3xl font-pixel font-bold mb-6 text-center flex justify-between items-center">
+              <div className="text-left">Log in to Pixelworld</div>
+              <span className="inline-block ml-2">🖌️</span>
+            </h1>
+            <form className="flex flex-col gap-4" onSubmit={handleLoginSubmit}>
+              <div>
+                <label className="block text-sm mb-1">Email Address</label>
+                <input
+                  type="email"
+                  className="w-full border border-gray-300 rounded-md p-2"
+                  placeholder="Email Address"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <label className="block text-sm mb-1">Password</label>
+                <input
+                  type="password"
+                  className="w-full border border-gray-300 rounded-md p-2"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <button
+                type="submit"
+                className="bg-gray-500 text-white rounded-md py-2 hover:bg-gray-600 border-1 border-gray-800 cursor-pointer"
+              >
+                Log in
+              </button>
+            </form>
 
-          <div className="my-4 text-center">
-            <button
-              onClick={() => {
-                onLoginWithGoogle(navigate);
-              }}
-              className="w-full flex items-center justify-center border border-gray-300 rounded-md py-2 hover:bg-gray-100 cursor-pointer px-2 whitespace-nowrap"
-            >
-              <i className="bi bi-google pr-2"></i>
-              Continue with Google
-            </button>
-          </div>
+            <div className="my-4 text-center">
+              <button
+                onClick={() => {
+                  onLoginWithGoogle(navigate);
+                }}
+                className="w-full flex items-center justify-center border border-gray-300 rounded-md py-2 hover:bg-gray-100 cursor-pointer px-2 whitespace-nowrap"
+              >
+                <i className="bi bi-google pr-2"></i>
+                Continue with Google
+              </button>
+            </div>
 
-          <div className="text-center mt-4">
-            <p className="text-sm mb-2 text-left">Are you new here?</p>
-            <button
-              onClick={() => setShowSignup(true)}
-              className="w-full bg-gray-500 text-white rounded-md py-2 hover:bg-gray-700 border-1 border-gray-800 cursor-pointer"
-            >
-              Create an account
-            </button>
-          </div>
-        </>
-      ) : (
-        <>
-          <h1 className="text-xl sm:text-3xl font-bold mb-6 text-center flex justify-between items-center w-full">
-            <div className="text-left">Create an account</div>
-            <span className="inline-block ml-2">🖌️</span>
-          </h1>
-          <form className="flex flex-col gap-4" onSubmit={handleSignupSubmit}>
-            <div>
-              <label className="block text-sm mb-1">Email Address</label>
-              <input
-                type="email"
-                className={`w-full border ${emailError ? "border-red-500" : "border-gray-300"} rounded-md p-2`}
-                placeholder="Email Address"
-                value={email}
-                onChange={handleEmailChange}
-                required
-              />
-              {emailError && (
-                <p className="text-red-500 text-xs mt-1">{emailError}</p>
-              )}
+            <div className="text-center mt-4">
+              <p className="text-sm mb-2 text-left">Are you new here?</p>
+              <button
+                onClick={() => setShowSignup(true)}
+                className="w-full bg-gray-500 text-white rounded-md py-2 hover:bg-gray-700 border-1 border-gray-800 cursor-pointer"
+              >
+                Create an account
+              </button>
             </div>
-            <div>
-              <label className="block text-sm mb-1">Password</label>
-              <input
-                type="password"
-                className={`w-full border ${passwordError || passwordMatchError ? "border-red-500" : "border-gray-300"} rounded-md p-2`}
-                placeholder="Password"
-                value={password}
-                onChange={handlePasswordChange}
-                required
-              />
-              {passwordError && (
-                <p className="text-red-500 text-xs mt-1">{passwordError}</p>
-              )}
-            </div>
-            <div>
-              <label className="block text-sm mb-1">Confirm Password</label>
-              <input
-                type="password"
-                className={`w-full border ${passwordMatchError ? "border-red-500" : "border-gray-300"} rounded-md p-2`}
-                placeholder="Confirm Password"
-                value={confirmPassword}
-                onChange={handleConfirmPasswordChange}
-                required
-              />
-              {passwordMatchError && (
-                <p className="text-red-500 text-xs mt-1">
-                  {passwordMatchError}
-                </p>
-              )}
-            </div>
-            <button
-              type="submit"
-              className="bg-gray-500 text-white rounded-md py-2 hover:bg-gray-600 border-1 border-gray-800 cursor-pointer"
-            >
-              Sign up
-            </button>
-          </form>
+          </>
+        ) : (
+          <>
+            <h1 className="text-xl sm:text-3xl font-bold mb-6 text-center flex justify-between items-center w-full">
+              <div className="text-left">Create an account</div>
+              <span className="inline-block ml-2">🖌️</span>
+            </h1>
+            <form className="flex flex-col gap-4" onSubmit={handleSignupSubmit}>
+              <div>
+                <label className="block text-sm mb-1">Email Address</label>
+                <input
+                  type="email"
+                  className={`w-full border ${emailError ? "border-red-500" : "border-gray-300"} rounded-md p-2`}
+                  placeholder="Email Address"
+                  value={email}
+                  onChange={handleEmailChange}
+                  required
+                />
+                {emailError && (
+                  <p className="text-red-500 text-xs mt-1">{emailError}</p>
+                )}
+              </div>
+              <div>
+                <label className="block text-sm mb-1">Password</label>
+                <input
+                  type="password"
+                  className={`w-full border ${passwordError || passwordMatchError ? "border-red-500" : "border-gray-300"} rounded-md p-2`}
+                  placeholder="Password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                  required
+                />
+                {passwordError && (
+                  <p className="text-red-500 text-xs mt-1">{passwordError}</p>
+                )}
+              </div>
+              <div>
+                <label className="block text-sm mb-1">Confirm Password</label>
+                <input
+                  type="password"
+                  className={`w-full border ${passwordMatchError ? "border-red-500" : "border-gray-300"} rounded-md p-2`}
+                  placeholder="Confirm Password"
+                  value={confirmPassword}
+                  onChange={handleConfirmPasswordChange}
+                  required
+                />
+                {passwordMatchError && (
+                  <p className="text-red-500 text-xs mt-1">
+                    {passwordMatchError}
+                  </p>
+                )}
+              </div>
+              <button
+                type="submit"
+                className="bg-gray-500 text-white rounded-md py-2 hover:bg-gray-600 border-1 border-gray-800 cursor-pointer"
+              >
+                Sign up
+              </button>
+            </form>
 
-          <div className="text-center mt-6">
-            <p className="text-sm mb-2">Already have an account?</p>
-            <button
-              onClick={() => setShowSignup(false)}
-              className="w-full bg-gray-500 text-white rounded-md py-2 hover:bg-gray-700 border-1 border-gray-800 cursor-pointer"
-            >
-              Back to login
-            </button>
-          </div>
-        </>
-      )}
+            <div className="text-center mt-6">
+              <p className="text-sm mb-2">Already have an account?</p>
+              <button
+                onClick={() => setShowSignup(false)}
+                className="w-full bg-gray-500 text-white rounded-md py-2 hover:bg-gray-700 border-1 border-gray-800 cursor-pointer"
+              >
+                Back to login
+              </button>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
