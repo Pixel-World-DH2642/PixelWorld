2a8078de51a6c21701c5c64115d74fbd322b63f3
GabkaMorska <kiryluk@kth.se>
2025-05-18 10:42:43 +0200

    added profiles

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -7,8 +7,8 @@ export function WelcomePage({ user, onLogout }) {
   const navigate = useNavigate();
 
   return (
-    <div className="font-pixel min-h-[calc(100vh-8rem)] flex flex-col">
-      <div>{user ? <NavBar className="w-full p-8 px-6 pt-6" /> : ""}</div>
+    <div className="font-pixel max-h-[80vh] overflow-y-auto flex flex-col px-9">
+      <div>{user ? <NavBar className="w-full p-8 px-6 pt-6 " /> : ""}</div>
       <div className="flex-grow flex flex-col justify-center items-center space-y-4">
         {user ? <h4></h4> : <h4>Welcome to</h4>}
         <h1 className="text-6xl">Pixel World</h1>
@@ -30,19 +30,73 @@ export function WelcomePage({ user, onLogout }) {
             </div>
           )}
         </h1>
-        <div>
-          <h3 className="px-8">
+        <div className="max-w-xl text-center">
+          <h3 className="px-10 text-sm sm:text-lg">
             Become a pixel artist! Pixel World allows you to paint pixelated
             images to later display them in a digital gallery. Accompanied by
             real-time weather and inspirational quotes.
           </h3>
         </div>
-        <div className="flex">
-          <img
-            src="public\assets\Krisztina portfolio.jpeg.jpg"
-            className=" flex w-13 h-13 sm:w-16 sm:h-16 aspect-square bg-gray-400 rounded-full border border-black "
-          ></img>
-          <h3>Krisztina Biro</h3>
+        <div className="">
+          About
+          <div className="flex pb-5">
+            <img
+              src="\assets\Krisztina portfolio.jpeg.jpg"
+              className=" flex w-13 h-13 sm:w-16 sm:h-16 aspect-square bg-gray-400 rounded-full border border-black "
+            ></img>
+            <div className="ml-4">
+              <h2 className="text-lg sm:text-xl font-semibold">
+                Krisztina Biro
+              </h2>
+              <div className="max-w-xl text-center">
+                <p className="text-sm text-gray-600 sm:text-lg">
+                  Designer of the Museum Page and the look and feel of the app!
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex pb-5">
+            <img
+              src="\assets\GABA.jpg"
+              className=" flex w-13 h-13 sm:w-16 sm:h-16 aspect-square bg-gray-400 rounded-full border border-black "
+            ></img>
+            <div className="ml-4">
+              <h2 className="text-lg sm:text-xl font-semibold">Gabi Kiryluk</h2>
+              <p className="text-sm text-gray-600">
+                Creator of the painting details page, the look and feel and the
+                welcome page!
+              </p>
+            </div>
+          </div>
+          <div className="flex pb-5">
+            <img
+              src="\assets\Krisztina portfolio.jpeg.jpg"
+              className=" flex w-13 h-13 sm:w-16 sm:h-16 aspect-square bg-gray-400 rounded-full border border-black "
+            ></img>
+            <div className="ml-4">
+              <h2 className="text-lg sm:text-xl font-semibold">
+                Krisztina Biro
+              </h2>
+              <div className="max-w-xl text-center">
+                <p className="text-sm text-gray-600 sm:text-lg">
+                  Designer of the Museum Page and the look and feel of the app!
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex pb-5">
+            <img
+              src="\assets\GABA.jpg"
+              className=" flex w-13 h-13 sm:w-16 sm:h-16 aspect-square bg-gray-400 rounded-full border border-black "
+            ></img>
+            <div className="ml-4">
+              <h2 className="text-lg sm:text-xl font-semibold">Gabi Kiryluk</h2>
+              <p className="text-sm text-gray-600">
+                Creator of the painting details page, the look and feel and the
+                welcome page!
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
