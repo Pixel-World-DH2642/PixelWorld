ff84cb5d2c4f41aa99a510b1661141a4d9cdbaf5
GabkaMorska <kiryluk@kth.se>
2025-05-19 15:40:33 +0200

    different ?

--- src/components/InstructionsCharacter.jsx
+++ src/components/InstructionsCharacter.jsx
@@ -5,3 +5,26 @@ export function InstructionsCharacter() {
     </div>
   );
 }
+
+export function InstructionsQuote() {
+  return (
+    <div className="border-2 border-black rounded-xl bg-white p-4 inline-block">
+      <p>
+        Are having a creative block? Generate a quote to inspire your next
+        creation. Include it in a submission to have it attached in your
+        painting submission.
+      </p>
+    </div>
+  );
+}
+
+export function InstructionsCanvas() {
+  return (
+    <div className="border-2 border-black rounded-xl bg-white p-4 inline-block">
+      <p>
+        Here you can paint your drawing. Missing a quote? Go back to the quote
+        notice board to get one.
+      </p>
+    </div>
+  );
+}
